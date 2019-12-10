96aae0d687cea789c99c018ee52a473d488e3067
MewW6m <aheadlink.changat@gmail.com>
2019-12-11 08:07:32 +0900

    191211

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
-(function ($) {
-"use strict";
+(function($) {
+	"use strict";
 	/*------------- preloader js --------------*/
-	$(window).on('load',function() { // makes sure the whole site is loaded
-		$('.preloder-wrap').fadeOut(); // will first fade out the loading animation
-		$('.loader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
-		$('body').delay(150).css({'overflow':'visible'})
-	})
-
-	/*--
-		Scroll Up
-	------------------------*/
+	$(window).on('load', function() { // makes sure the whole site is loaded
+			$('.preloder-wrap').fadeOut(); // will first fade out the loading animation
+			$('.loader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
+			$('body').delay(150).css({
+				'overflow': 'visible'
+			})
+		})
+		/*--
+			Scroll Up
+		------------------------*/
 	$.scrollUp({
 		easingType: 'linear',
 		scrollSpeed: 900,
 		animation: 'fade',
 		scrollText: '<i class="fa fa-chevron-up"></i>',
 	});
-
-})(jQuery);	
-
+})(jQuery);
 // 置き換え
 var spans = document.getElementById("intro-spans");
-if(window.innerWidth < 480){
+if(window.innerWidth < 480) {
 	spans.innerHTML = spans.innerHTML.replace(/>\/</g, "><br><")
 }
 // Chart.js
@@ -40,18 +39,17 @@ var colorSet = {
 	grey: 'rgb(201, 203, 207)'
 };
 var color = Chart.helpers.color;
-
 var defaultSettings = {
-    type: 'radar',
-    data: {
-    	datasets: [{
+	type: 'radar',
+	data: {
+		datasets: [{
 			label: "Working(Year)",
 			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
 			borderColor: colorSet.red,
 			borderWidth: 1,
 			pointRadius: 1,
 			pointBackgroundColor: colorSet.red,
-		},{
+		}, {
 			label: "Private(Year)",
 			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
 			borderColor: colorSet.blue,
@@ -59,19 +57,19 @@ var defaultSettings = {
 			pointRadius: 1,
 			pointBackgroundColor: colorSet.blue,
 		}]
-    },
-    options: {
+	},
+	options: {
 		title: {
-                display: true,
-                fontSize: 15,
-				fontColor: "white"
-        },
-        legend: {
-            position: 'bottom',
+			display: true,
+			fontSize: 15,
+			fontColor: "white"
+		},
+		legend: {
+			position: 'bottom',
 			labels: {
 				fontColor: "white"
 			}
-        },
+		},
 		scale: {
 			ticks: {
 				display: true,
@@ -80,51 +78,67 @@ var defaultSettings = {
 				fontColor: "white",
 				backdropColor: 'transparent' // should render black behind the text
 			},
-            pointLabels: {
-                fontSize: 10,         
-                fontColor: "white"    
-            },
-			angleLines: {	
+			pointLabels: {
+				fontSize: 10,
+				fontColor: "white"
+			},
+			angleLines: {
 				display: true,
 				color: "orange"
 			},
 			gridLines: {
 				display: true,
 				color: "orange"
-			}	
+			}
 		},
 		gridLines: {
-   			display: false
+			display: false
 		},
 		responsive: true,
 		maintainAspectRatio: true
-    }
+	}
 }
-
 var setting1 = JSON.parse(JSON.stringify(defaultSettings));
 setting1.options.title.text = "ProgrammingLanguage"
-setting1.data.labels = ['HTML/CSS/JS', 'PHP', 'Python', 'VB.net', 'JAVA','C#', 'C/C++', 'Perl']
-setting1.data.datasets[0].data = [0,0,0,2,0.5,0,0,0]
-setting1.data.datasets[1].data = [4,1,3,2,1,0.5,1,1]
+setting1.data.labels = ['HTML/CSS/JS', 'PHP', 'Python', 'VB.net', 'JAVA', 'C#', 'C/C++', 'Perl']
+setting1.data.datasets[0].data = [2, 0, 0, 2, 0.5, 0, 0, 0]
+setting1.data.datasets[1].data = [4, 1, 3, 2, 1, 0.5, 1, 1]
 var myRadarChart1 = new Chart(ctx1, setting1);
-
 var setting2 = JSON.parse(JSON.stringify(defaultSettings));
 setting2.options.title.text = "FrameWork"
-setting2.data.labels = ['Django', 'Flask', 'Jquery', 'Node','Bootstrap', 'CSS/JS Framework', 'Python Framework', 'ASP.net', "Spring"]
+setting2.data.labels = ['Django', 'Flask', 'Jquery', 'Node', 'Bootstrap', 'CSS/JS Framework', 'Python Framework', 'ASP.net', "Spring"]
 setting2.data.datasets[0].data = [0, 0, 0, 0, 0, 0, 0, 1, 0.5]
 setting2.data.datasets[1].data = [2, 1, 1, 0.5, 1, 1, 3, 1, 0]
 var myRadarChart2 = new Chart(ctx2, setting2);
-
 var setting3 = JSON.parse(JSON.stringify(defaultSettings));
 setting3.options.title.text = "AppPlatform"
 setting3.data.labels = ['Web', 'Android', 'Linux', 'WindowsServer', 'WindowsForm']
 setting3.data.datasets[0].data = [2, 0, 0, 2, 1]
 setting3.data.datasets[1].data = [4, 1, 3, 1, 0]
 var myRadarChart3 = new Chart(ctx3, setting3);
-
 var setting4 = JSON.parse(JSON.stringify(defaultSettings));
 setting4.options.title.text = "DevTools"
-setting4.data.labels = ['Git', 'SVN','Docker', 'Hyper-V', 'VMWare/VirtualBox', 'Shell', 'cmd/PS', 'Vim']
+setting4.data.labels = ['Git', 'SVN', 'Docker', 'Hyper-V', 'VMWare/VirtualBox', 'Shell', 'cmd/PS', 'Vim']
 setting4.data.datasets[0].data = [0, 2, 0.5, 2, 0, 0, 2, 0]
 setting4.data.datasets[1].data = [3, 0, 0, 1, 1, 4, 1, 3]
 var myRadarChart4 = new Chart(ctx4, setting4);
+$('.bio-charts .f_section_title').on('click', function() {
+	$('.bio-charts .f_section_title').html("Will Skill");
+	defaultSettings.optioins.legend.scale.ticks.max = 10
+	var setting1 = JSON.parse(JSON.stringify(defaultSettings));
+	var setting2 = JSON.parse(JSON.stringify(defaultSettings));
+	var setting3 = JSON.parse(JSON.stringify(defaultSettings));
+	var setting4 = JSON.parse(JSON.stringify(defaultSettings));
+	setting1.data.datasets[0].data = [4, 0, 0, 2, 0.5, 0, 0, 0]
+	setting1.data.datasets[1].data = [4, 1, 3, 2, 1, 0.5, 1, 1]
+	setting2.data.datasets[0].data = [0, 0, 0, 0, 0, 0, 0, 1, 0.5]
+	setting2.data.datasets[1].data = [2, 1, 1, 0.5, 1, 1, 3, 1, 0]
+	setting3.data.datasets[0].data = [2, 0, 0, 2, 1]
+	setting3.data.datasets[1].data = [4, 1, 3, 1, 0]
+	setting4.data.datasets[0].data = [0, 2, 0.5, 2, 0, 0, 2, 0]
+	setting4.data.datasets[1].data = [3, 0, 0, 1, 1, 4, 1, 3]
+	var myRadarChart1 = new Chart(ctx1, setting1);
+	var myRadarChart2 = new Chart(ctx2, setting2);
+	var myRadarChart3 = new Chart(ctx3, setting3);
+	var myRadarChart4 = new Chart(ctx4, setting4);
+});
