f47344805f00615d99f386a370752e6f4e8bd60b
MewW6m <aheadlink.changat@gmail.com>
2021-05-01 12:30:16 +0900

    210501_3

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ var defaultSettings = {
 		scale: {
 			ticks: {
 				display: true,
-				max: 4.5,
+				max: 6,
 				maxTicksLimit: 10,
 				fontColor: "white",
 				backdropColor: 'transparent' // should render black behind the text
